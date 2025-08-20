cb6fd4093ad6f758be6f3e47ca9725aee37bbd85
Subhasis Panda <subhasispanda1256700@gmail.com>
2025-08-20 15:30:00 +0530

    import to fix TypeError in summarize route

--- backend/routes/summarize.js
+++ backend/routes/summarize.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const axiosRetry = require('axios-retry');
+const axiosRetry = require('axios-retry').default;
 const validator = require('validator');
 const { summarizeSchema } = require('../utils/validation');
 const { ValidationError, ExternalAPIError } = require('../utils/errors');
